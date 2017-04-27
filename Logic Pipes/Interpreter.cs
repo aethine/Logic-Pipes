f1ce1e607c3f2a23af82462b0e263164b4cbc4bd
Kaleb Shane <yepicness@gmail.com>
2017-04-27 10:38:48 -0400

    Designed the prompt and added interpreter class as well as output function

---
+++ Logic Pipes/Interpreter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Pipes
+{
+    static class Interpreter
+    {
+        static void Output(string message)
+        {
+            Program.Prompt.Output.Text += message + Environment.NewLine;
+        }
+    }
+}
--- Logic Pipes/Prompt.Designer.cs
+++ Logic Pipes/Prompt.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Enter = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(11, 983);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(827, 31);
+            this.Input.TabIndex = 0;
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(587, 1029);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(253, 41);
+            this.Enter.TabIndex = 1;
+            this.Enter.Text = "Enter";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(13, 12);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Output.Size = new System.Drawing.Size(827, 954);
+            this.Output.TabIndex = 2;
+            // 
+            // Prompt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1661, 719);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(850, 1086);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.Input);
+            this.Name = "Prompt";
+            this.Text = "Logic Prompt";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button Enter;
+        public System.Windows.Forms.TextBox Input;
+        public System.Windows.Forms.TextBox Output;
     }
 }
 
