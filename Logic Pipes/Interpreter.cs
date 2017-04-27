f5d4b7510923316d574b209e3f47a265ee35dc27
Kaleb Shane <yepicness@gmail.com>
2017-04-27 19:13:15 -0400

    Added basic pipe functionality and output pipe

--- Logic Pipes/Interpreter.cs
+++ Logic Pipes/Interpreter.cs
@@ -9,14 +9,15 @@ namespace Logic_Pipes
     class Pipe
     {
         public string Name;
-        public bool System = false;
+        public bool Sys = false;
         public Pipe(string name) { Name = name; }
     }
     class Container
     {
         public string Path;
-        public bool System = false;
+        public bool Sys = false;
         public List<Pipe> Attatched;
+        public void Add(string contents) { System.IO.File.AppendAllText(Path, contents + Environment.NewLine); }
         public Container(string path, params Pipe[] attatch) { Path = path; Attatched = attatch.ToList(); }
     }
     class Engine //todo
@@ -31,13 +32,65 @@ namespace Logic_Pipes
     }
     static class Interpreter
     {
+        static string[] words;
+        public static List<Pipe> Pipes = new List<Pipe>();
+        public static List<Container> Containers = new List<Container>();
+        public static List<Engine> Engines = new List<Engine>();
+        
+        static string getAllAfter(int after)
+        {
+            string o = null;
+            for (int x = after + 1; x < words.Length; x++) o += words[x] + " ";
+            return o;
+        }
         static void Output(string message)
         {
             Program.Prompt.Output.Text += message + Environment.NewLine;
         }
+        public static Pipe FindPipeByName(string name)
+        {
+            foreach (Pipe p in Pipes)
+                if (p.Name == name) return p;
+            throw new ArgumentException();
+        }
+        public static void SendDownPipe(Pipe pipe, string contents)
+        {
+            List<Container> cs = new List<Container>();
+            foreach (Container c in Containers)
+                foreach (Pipe p in c.Attatched)
+                {
+                    if (p == pipe)
+                    {
+                        if (c.Path == "$output") Output(contents);
+                        else c.Add(contents);
+                    }
+                }
+        }
+        public static void Sysinit()
+        {
+            Pipes.Add(new Pipe("Output"));
+            Containers.Add(new Container("$output"));
+            Pipes[0].Sys = true;
+            Containers[0].Sys = true;
+            Containers[0].Attatched.Add(Pipes[0]);
+        }
         public static void Interpret(string line)
         {
-            string[] words = line.Split(' ');
+            words = line.Split(' ');
+            if(words[0] == "pipe")
+            {
+                if (words.Length >= 3)
+                {
+                    string[] pipes;
+                    if (words[1].Contains(','))
+                        pipes = words[1].Split(',');
+                    else pipes = new string[] { words[1]};
+                    foreach (string s in pipes)
+                        try { SendDownPipe(FindPipeByName(s), getAllAfter(1)); }
+                        catch (ArgumentException) { Output("[SYS] Could not find pipe " + s); }
+                }
+                else Output("[SYS] Not enough parameters");
+            }
         }
     }
 }
