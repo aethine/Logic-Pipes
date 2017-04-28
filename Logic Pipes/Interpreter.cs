76b666f2e6e74e61165a9043037c603c794e8aa0
Kaleb Shane <yepicness@gmail.com>
2017-04-27 20:50:50 -0400

    Added commands to create files, pipes, and containers; also to attatch pipes to containers

--- Logic Pipes/Interpreter.cs
+++ Logic Pipes/Interpreter.cs
@@ -14,11 +14,12 @@ namespace Logic_Pipes
     }
     class Container
     {
+        public string Name;
         public string Path;
         public bool Sys = false;
         public List<Pipe> Attatched;
         public void Add(string contents) { System.IO.File.AppendAllText(Path, contents + Environment.NewLine); }
-        public Container(string path, params Pipe[] attatch) { Path = path; Attatched = attatch.ToList(); }
+        public Container(string name, string path, params Pipe[] attatch) { Name = name; Path = path; Attatched = attatch.ToList(); }
     }
     class Engine //todo
     {
@@ -53,6 +54,12 @@ namespace Logic_Pipes
                 if (p.Name == name) return p;
             throw new ArgumentException();
         }
+        public static Container FindContByName(string name)
+        {
+            foreach (Container c in Containers)
+                if (c.Name == name) return c;
+            throw new ArgumentException();
+        }
         public static void SendDownPipe(Pipe pipe, string contents)
         {
             List<Container> cs = new List<Container>();
@@ -61,35 +68,106 @@ namespace Logic_Pipes
                 {
                     if (p == pipe)
                     {
-                        if (c.Path == "$output") Output(contents);
+                        if (c.Name == "$output") Output(contents);
                         else c.Add(contents);
+                        break;
                     }
                 }
         }
         public static void Sysinit()
         {
             Pipes.Add(new Pipe("Output"));
-            Containers.Add(new Container("$output"));
+            Containers.Add(new Container("$output", null));
             Pipes[0].Sys = true;
             Containers[0].Sys = true;
             Containers[0].Attatched.Add(Pipes[0]);
         }
         public static void Interpret(string line)
         {
-            words = line.Split(' ');
-            if(words[0] == "pipe")
+            if (!string.IsNullOrWhiteSpace(line))
             {
-                if (words.Length >= 3)
+                words = line.Split(' ');
+                if (words[0] == "pipe")
                 {
-                    string[] pipes;
-                    if (words[1].Contains(','))
-                        pipes = words[1].Split(',');
-                    else pipes = new string[] { words[1]};
-                    foreach (string s in pipes)
-                        try { SendDownPipe(FindPipeByName(s), getAllAfter(1)); }
-                        catch (ArgumentException) { Output("[SYS] Could not find pipe " + s); }
+                    if (words.Length >= 3)
+                    {
+                        string[] pipes;
+                        if (words[1].Contains(','))
+                            pipes = words[1].Split(',');
+                        else pipes = new string[] { words[1] };
+                        foreach (string s in pipes)
+                            try { SendDownPipe(FindPipeByName(s), getAllAfter(1)); }
+                            catch (ArgumentException) { Output("[SYS] Could not find pipe " + s); }
+                    }
+                    else Output("[SYS] Not enough parameters");
                 }
-                else Output("[SYS] Not enough parameters");
+                else if (words[0] == "mktainer")
+                {
+                    if (words.Length >= 3)
+                    {
+                        bool f = true;
+                        foreach (Container c in Containers)
+                        {
+                            if (c.Name == words[1]) { Output("[SYS] container already exists with given name"); f = false; break; }
+                            if (c.Path == words[2]) { Output("[SYS] container already exists on given path"); f = false; break; }
+                        }
+                        if (f)
+                        {
+                            if (System.IO.File.Exists(words[2]))
+                            { Containers.Add(new Container(words[1], words[2]));
+                              Output("[SYS] Created a container named " + words[1] + " at " + words[2]); }
+                            else Output("[SYS] Could not find file given");
+                        }
+                    }
+                    else Output("[SYS] Not enough parameters");
+
+                }
+                else if (words[0] == "mkpipe")
+                {
+                    if (words.Length >= 2)
+                    {
+                        bool f = true;
+                        foreach(Pipe p in Pipes)
+                            if(p.Name == words[1]) { Output("[SYS] pipe already exists with given name"); f = false; break; }
+                        if (f) { Pipes.Add(new Pipe(words[1])); Output("[SYS] Created a pipe called " + words[1]); }
+                    }
+                    else Output("[SYS] Not enough parameters");
+                }
+                else if (words[0] == "attatch")
+                {
+                    if(words.Length >= 3)
+                    {
+                        Container c;
+                        Pipe p;
+                        try
+                        {
+                            p = FindPipeByName(words[1]);
+                            try
+                            {
+                                c = FindContByName(words[2]);
+                                c.Attatched.Add(p);
+                                Output("[SYS] attatched pipe " + words[1] + " to container " + words[2]);
+                            }
+                            catch (ArgumentException) { Output("[SYS] Container not found"); }
+                        }
+                        catch (ArgumentException) { Output("[SYS] Pipe not found"); }
+                    }
+                    else Output("[SYS] Not enough parameters");
+                }
+                else if (words[0] == "mkfile")
+                {
+                    if(words.Length >= 2)
+                    {
+                        if (!System.IO.File.Exists(words[1]))
+                        {
+                            try { System.IO.File.WriteAllText(words[1], null); Output("[SYS] Created " + words[1]); }
+                            catch (System.IO.IOException) { Output("[SYS] Path not valid"); }
+                        }
+                        else Output("[SYS] File already exists");
+                    }
+                    else Output("[SYS] Not enough parameters");
+                }
+                else Output("[SYS] Command " + words[0] + " not found");
             }
         }
     }
