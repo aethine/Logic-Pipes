6dd68efa132ba98de1c3ec92f4f3b0f825fddd27
Kaleb Shane <yepicness@gmail.com>
2017-05-01 10:24:38 -0400

    Added variables, to be like batch variables

---
+++ Logic Pipes/Memory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Pipes
+{
+    static class Memory
+    {
+        private class Var
+        {
+            public string name, value;
+            public Var(string name, string value) { this.name = name; this.value = value; }
+        }
+        static List<Var> Vars = new List<Var>();
+        static Var FindVar(string name)
+        {
+            foreach (Var v in Vars) if (v.name == name) return v;
+            return null;
+        }
+        static bool VarExists(string name)
+        {
+            return FindVar(name) != null;
+        }
+        static bool VarExists(Var v)
+        {
+            return VarExists(v.name);
+        }
+        public static void NewVar(string name, string value) { if(!VarExists(name)) Vars.Add(new Var(name, value)); }
+        public static void DelVar(string name) { Vars.Remove(FindVar(name)); }
+        public static void SetVar(string name, string newvalue)
+        {
+            if (VarExists(name))
+            {
+                foreach (Var v in Vars)
+                    if (v.name == name) v.value = newvalue;
+            }
+        }
+        public static string GetVar(string name)
+        {
+            if (VarExists(name))
+                return FindVar(name).value;
+            else return null;
+        }
+    }
+}
